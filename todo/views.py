79de4f2f93e6ba595676d0ff1a3bf633733fd5c4
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-06-13 10:38:03 +0000

    Item loop added, Items defined

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
+
+# render the item, define the context vocabular, then add as a return element 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
- 
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html',context)
