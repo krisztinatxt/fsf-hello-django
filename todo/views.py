bdce3059929cf4816e523499fd169195b21e2ee9
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-06-14 08:15:01 +0000

    Add item form with Django added

--- todo/views.py
+++ todo/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render, redirect
 from .models import Item
+from .forms import ItemForm
+
+
 # Create your views here.
 
-# render the item, define the context vocabular, then add as a return element 
+# render the item, define the context vocabular, then add as a return element
+
+
 def get_todo_list(request):
     items = Item.objects.all()
     context = {
         'items': items
     }
-    return render(request, 'todo/todo_list.html',context)
+    return render(request, 'todo/todo_list.html', context)
+
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
-        return redirect('get_todo_list')
-    return render(request, 'todo/add_item.html')
-
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+            'form': form
+        }
+    return render(request, 'todo/add_item.html', context)
